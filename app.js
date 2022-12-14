bc653e0e5173eb1ce5d8bd97157960620ad2f467
Gavin <77718662+gavinwale@users.noreply.github.com>
2022-12-13 23:51:05 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const Web3 = require('web3');
-const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/3010d569218a4a7b9ca00fcfb8fde31e'));
+const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/APIKEY'));
 
 function flipCoin() {
     // Generate a random number between 0 and 1
