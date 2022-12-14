789ab917651f75430c1f448cb608beba18994a4c
Gavin <gavinwale@u.boisestate.edu>
2022-12-14 00:17:53 -0600

    Still tryin

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('https://mainnet.infura.io/v3/3010d569218a4a7b9ca00fcfb8fde31e'));
 
+const tokenContract = "0x6b175474e89094c44da98b954eedeac495271d0f";
+
 function flipCoin() {
     // Generate a random number between 0 and 1
     // 0 represents heads and 1 represents tails
@@ -29,10 +31,22 @@ async function connect() {
         const walletAddress = account.givenProvider.selectedAddress;
         console.log(`Wallet: ${walletAddress}`);
         document.getElementById("walletAddress").innerHTML = walletAddress;
+        let balance = getTokenBalance();
+        document.getElementById("balance").innerHTML = balance;
     } else {
         console.log("No wallet");  
     }
 }
+
+const abi = require('./dgnw.json').abi;
+const contract = new web3.eth.Contract(abi, tokenContract);
+
+async function getTokenBalance() {
+    let result =  await contract.methods.balanceOf(walletAddress).call();
+    const formattedResult = web3.utils.fromWei(result, "ether");
+    return formattedResult;
+}
+
 
 // const address = '0x4989b5Ace8e387AfCC2c3cFEc697F2459d705bD7';
 // const abi = require('./dgnw.json').abi;
